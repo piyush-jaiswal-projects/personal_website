faae46d39a6e3c61e441e9cf09c2d66f40260313
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2024-03-12 17:12:10 +0530

    refactored api

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,5 +1,5 @@
 import { AppDataSource, connectDb, disconnectDb } from "@/db";
-import { ArticleSchema, ProjectSchema } from "@/models";
+import { ProjectSchema } from "@/models";
 import { ApiError, ApiResponse, getErrorMessage, logError } from "@/utils";
 import { NextApiRequest, NextApiResponse } from "next";
 
@@ -10,54 +10,13 @@ export default async function handler(
   try {
     await connectDb();
 
-    if (req.method === "GET") {
-      const projects = await AppDataSource.getRepository(ProjectSchema).find(
-        {},
-      );
-      res
-        .status(200)
-        .json(
-          new ApiResponse(200, projects, "Data fetched successfully!", true),
-        );
-    }
+    if (req.method === "GET") await getRequest(req, res);
 
-    if (req.method === "POST") {
-      const project = await AppDataSource.getRepository(ProjectSchema).create(
-        req.body,
-      );
-      const result =
-        await AppDataSource.getRepository(ProjectSchema).save(project);
+    if (req.method === "POST") await postRequest(req, res);
 
-      if (!result) throw Error("Error creating project!");
+    if (req.method === "DELETE") await deleteRequest(req, res);
 
-      res
-        .status(200)
-        .json(new ApiResponse(200, result, "Project created!", true));
-    }
-
-    if (req.method === "DELETE") {
-      const { id } = req.query;
-
-      const project = await AppDataSource.getRepository(ProjectSchema).delete({
-        id: id,
-      });
-      if (project.affected === 0) throw Error("Error deleting project!");
-
-      res.status(200).json(new ApiResponse(200, "", "Project deleted!", true));
-    }
-
-    if (req.method === "PUT") {
-      const { id } = req.query;
-      const body = req.body;
-
-      const project = await AppDataSource.getRepository(ProjectSchema).update(
-        { id: id },
-        body,
-      );
-      if (project.affected === 0) throw Error("Error updating project!");
-
-      res.status(200).json(new ApiResponse(200, "", "Project updated!", true));
-    }
+    if (req.method === "PUT") await putRequest(req, res);
 
     res.status(404).json(new ApiError(404, "Invalid request!"));
   } catch (error) {
@@ -68,3 +27,49 @@ export default async function handler(
     disconnectDb();
   }
 }
+
+const getRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("executing get request");
+  const projects = await AppDataSource.getRepository(ProjectSchema).find({});
+  res
+    .status(200)
+    .json(new ApiResponse(200, projects, "Data fetched successfully!", true));
+};
+
+const postRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("executing post request");
+  const project = await AppDataSource.getRepository(ProjectSchema).create(
+    req.body,
+  );
+  const result = await AppDataSource.getRepository(ProjectSchema).save(project);
+
+  if (!result) throw Error("Error creating project!");
+
+  res.status(200).json(new ApiResponse(200, result, "Project created!", true));
+};
+
+const deleteRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("executing delete request");
+  const { id } = req.query;
+
+  const project = await AppDataSource.getRepository(ProjectSchema).delete({
+    id: id,
+  });
+  if (project.affected === 0) throw Error("Error deleting project!");
+
+  res.status(200).json(new ApiResponse(200, "", "Project deleted!", true));
+};
+
+const putRequest = async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log("executing put request");
+  const { id } = req.query;
+  const body = req.body;
+
+  const project = await AppDataSource.getRepository(ProjectSchema).update(
+    { id: id },
+    body,
+  );
+  if (project.affected === 0) throw Error("Error updating project!");
+
+  res.status(200).json(new ApiResponse(200, "", "Project updated!", true));
+};
