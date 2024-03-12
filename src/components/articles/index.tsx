288fd6baa7b4aefff5686ee4b564c112d3579fdb
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2024-03-12 16:32:13 +0530

    deploying apis first:

--- src/components/articles/index.tsx
+++ src/components/articles/index.tsx
@@ -5,19 +5,19 @@ import { req, sendHttpRequest } from "@/utils";
 import { ArticleCardProps } from "@/types/article";
 
 const Articles = async (props: { minCount: boolean }) => {
-  const articlesList = await sendHttpRequest("blogs", req.get);
+  // const articlesList = await sendHttpRequest("blogs", req.get);
 
   return (
     <div>
       <SectionTitle title="Recent writings" />
-      {articlesList.data
+      {/* {articlesList.data
         .slice(0, props.minCount === true ? 3 : articlesList.length)
         .map((article: ArticleCardProps, index: number) => {
           return <ArticleCard key={`article-${index}`} {...article} />;
         })}
       <div className="mt-4 text-center">
         <NavLink path="/articles" text="view more" />
-      </div>
+      </div> */}
     </div>
   );
 };
--- src/components/projects/index.tsx
+++ src/components/projects/index.tsx
@@ -5,19 +5,19 @@ import sendApiRequest, { req } from "@/utils/sendHttpRequest";
 import { ProjectCardProps } from "@/types";
 
 const Projects = async (props: { minCount: boolean }) => {
-  const projectsList = await sendApiRequest("projects", req.get);
+  // const projectsList = await sendApiRequest("projects", req.get);
 
   return (
     <div>
       <SectionTitle title="Projects" />
-      {projectsList.data
+      {/* {projectsList.data
         .slice(0, props.minCount === true ? 3 : projectsList.length)
         .map((project: ProjectCardProps, index: number) => {
           return <ProjectCard key={`project-${index}`} {...project} />;
         })}
       <div className="mt-4 text-center">
         <NavLink path="/projects" text="view more" />
-      </div>
+      </div> */}
     </div>
   );
 };
